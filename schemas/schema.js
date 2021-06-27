c7d7d1f5b3e3e1d2e20964cdc098d63bc3205ee7
ClarkMitchell <clarkmitchelldev@gmail.com>
2021-06-27 16:48:26 -0400

    add schemas

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,8 +1,18 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
-
+import createSchema from 'part:@sanity/base/schema-creator';
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+// We import object and document schemas
+import callToAction from './callToAction';
+import form from './form';
+import hero from './hero';
+import imageGallery from './imageGallery';
+import page from './page';
+import service from './service';
+import services from './services';
+import textWithIllustration from './textWithIllustration';
+import video from './video';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,6 +21,14 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    callToAction,
+    form,
+    hero,
+    imageGallery,
+    page,
+    service,
+    services,
+    textWithIllustration,
+    video
   ]),
 })
